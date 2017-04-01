3d3d6490d4a1998f51ec00347f3628fbcde91c02
Geronimo Poppino <gresco@gmail.com>
2017-04-01 02:18:56 -0300

    Added a better way to handle version number

--- src/main/java/com/github/NewsBotIRC/cmds/Version.java
+++ src/main/java/com/github/NewsBotIRC/cmds/Version.java
@@ -2,6 +2,7 @@ package com.github.NewsBotIRC.cmds;
 
 import com.github.NewsBotIRC.ConfReader;
 import com.github.NewsBotIRC.IRCMediator;
+import org.apache.commons.configuration.Configuration;
 
 public class Version implements Cmd {
 
@@ -14,6 +15,11 @@ public class Version implements Cmd {
     @Override
     public void action(IRCMediator m, String params)
     {
-        m.showMessage(ConfReader.getInstance().getVersion());
+        Configuration config = ConfReader.getAppProperties();
+        m.showMessage(config.getString("application.name") + " v" +
+                config.getString("application.version") + " by " +
+                config.getString("application.developers") + " (" +
+                config.getString("application.inceptionYear") + ")"
+        );
     }
 }
